2190dc0845e0dfaa95d51e8f680fa187bee5955e
cewthen-hue <cewthen@gmail.com>
2026-07-21 12:50:01 +0200

    Met à jour le cache avec le correctif de saut

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_NAME = "rightbound-shell-v0.4.3";
+const CACHE_NAME = "rightbound-shell-v0.4.4";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -8,9 +8,10 @@ const APP_SHELL = [
   "./styles/game.css?v=0.2.1",
   "./styles/menu.css?v=0.3.0",
   "./styles/app.css?v=0.4.0",
-  "./styles/fixes.css?v=0.4.2",
+  "./styles/fixes.css?v=0.4.3",
   "./src/game.js?v=0.2.1",
   "./src/meta-menu.js?v=0.4.0",
+  "./src/health-sync.js?v=0.4.3",
   "./src/app-shell.js?v=0.4.0",
   "./assets/icons/icon.svg",
   "./assets/icons/maskable.svg"
